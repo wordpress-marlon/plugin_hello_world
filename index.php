d4c41e8ecb93bbfc97223bd8047d02560461b1b4
marlon <falconsoft.3d@gmail.com>
2020-09-27 14:02:58 +0200

    sc

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+   Plugin Name: Hello world
+   Pluging URI: https://marlonfalcon.cl
+   Description: Plugin Demo de Wordpress
+   Author: Marlon Falcon
+   Version: 1.0
+   Author URI: https://marlonfalcon.cl
+*/
+?>
